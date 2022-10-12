543abd1fdd22b1f76a162596625c83828d4f33c5
gustavokmm <gustavo.uoft@gmail.com>
2022-10-12 09:20:10 -0300

    Desafio 2: Eventos de Customer

--- src/domain/@shared/event/event-dispatcher.spec.ts
+++ src/domain/@shared/event/event-dispatcher.spec.ts
@@ -1,3 +1,8 @@
+import CustomerAddressChangedEvent from "../../customer/event/customer-address-changed-event";
+import CustomerCreatedEvent from "../../customer/event/customer-created.event";
+import EnviaConsoleLog1Handler from "../../customer/event/handler/first-action-when-customer-is-created.handler";
+import EnviaConsoleLog2Handler from "../../customer/event/handler/second-action-when-customer-is-created.handler";
+import EnviaConsoleLogHandler from "../../customer/event/handler/third-action-when-customer-address-is-changed";
 import SendEmailWhenProductIsCreatedHandler from "../../product/event/handler/send-email-when-product-is-created.handler";
 import ProductCreatedEvent from "../../product/event/product-created.event";
 import EventDispatcher from "./event-dispatcher";
@@ -79,4 +84,115 @@ describe("Domain events tests", () => {
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
+
+  it("should register first event handler for customer", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new EnviaConsoleLog1Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler);
+  });
+
+  it("should register second event handler for customer", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new EnviaConsoleLog2Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler);
+  });
+
+  it("should notify all event handlers for customer created event", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new EnviaConsoleLog1Handler();
+    const eventHandler2 = new EnviaConsoleLog2Handler();
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(2);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(eventHandler2);
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      name: "Customer 1"
+    });
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
+  });
+
+  it("should unregister all event handlers for customer created event", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new EnviaConsoleLog1Handler();
+    const eventHandler2 = new EnviaConsoleLog2Handler();
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"].length).toBe(2);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(eventHandler2);
+
+    eventDispatcher.unregisterAll();
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"]).toBeUndefined();
+  });
+
+  
+  it("should register change address event handler for customer address changed", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new EnviaConsoleLogHandler();
+
+    eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"]).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"][0]).toMatchObject(eventHandler);
+  });
+
+  it("should notify all event handlers for customer address changed", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new EnviaConsoleLogHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"][0]).toMatchObject(eventHandler);
+
+    const customerCreatedEvent = new CustomerAddressChangedEvent({
+      id: "1234",
+      nome: "Customer 1",
+      endereco: "Novo endereço alterado"
+    });
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+
+  it("should unregister all event handlers for customer address changed event", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new EnviaConsoleLogHandler();
+
+    eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"].length).toBe(1);
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"][0]).toMatchObject(eventHandler);
+
+    eventDispatcher.unregisterAll();
+
+    expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"]).toBeUndefined();
+  });
+
 });
